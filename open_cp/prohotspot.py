1ea24b7f0e74da92a30f072c8caada8314e3e737
Matthew Daws <matt.daws@cantab.net>
2017-04-07 09:47:47 +0100

    Refactor prohotspot

--- open_cp/prohotspot.py
+++ open_cp/prohotspot.py
@@ -1,83 +1,161 @@
+"""
+prohotspot
+~~~~~~~~~~
+
+Implements the "prospective hotspotting" technique from:
+
+1. Bowers, Johnson, Pease,
+   "Prospective hot-spotting: The future of crime mapping?",
+   Brit. J. Criminol. (2004) 44 641--658.  doi:10.1093/bjc/azh036
+2. Johnson et al.,
+   "Prospective crime mapping in operational context",
+   Home Office Online Report 19/07
+   [Police online library](http://library.college.police.uk/docs/hordsolr/rdsolr1907.pdf)
+
+The underlying idea is to start with a kernel / weight defined in space and
+positive time.  This typically has finite extent, and might be related to
+discretised space and/or time.  Weights used in the literature tend to be
+of the form :math 1/(1+d):
+
+The classical algorithm assigns all events to cells in a gridding of space,
+and a "grid" of time (typically the number of whole weeks before the current
+time).  Only events which are close enough in space and time to the grid cell
+of interest are used.  For these, the weight is evaluated on each one, and then
+the sum taken.
+
+It is important to note the coupling between the grid size used and the weight,
+because it is the distance between grid cells which is used.  Exactly what
+"distance" here means is unclear, and we have provided a number of options.
+
+Alternatively, we can just use the weight / kernel in a continuous kernel
+density estimate scheme.
+"""
+
 from . import predictors
 from . import data
 
 import abc as _abc
 import numpy as _np
+from numpy import timedelta64
 
 class Weight(metaclass=_abc.ABCMeta):
+    """Base class for weights / kernels.  Classes implementing this algorithm
+    are responsible purely for providing weights.  We leave the details of
+    possibly discretising data to other classes.
+    """
+
     @_abc.abstractmethod
-    def __call__(self, cell, timestamp, x, y):
+    def __call__(self, dt, dd):
+        """Evaluate the weight given the potentially discretised input.
+
+        :param dt: The time distance from 0.  May be a scalar or a numpy array;
+        should be of a number type, not `timedelta` or similar.
+        :param dd: Spatial distance.  May be a scalar or a one-dimensional
+        numpy array.
+
+        :return: A scalar or one-dimensional numpy array as appropriate.
+        """
         pass
 
-class ClassicDiagonalsSame(Weight):
+
+class ClassicWeight(Weight):
+    """The classical weight, :math (1/(1+d))(1/(1+t)): where :math d: is
+    distance and :math t: is time.  Default units are "grid cells" and "weeks",
+    respectively.
+
+    :param space_bandwidth: Distances greater than or equal to this set the
+    weight to 0.
+    :param time_bandwidth: Times greater than or equal to this set the weight
+    to 0.
+    """
     def __init__(self):
-        self.space_bandwidth = 400
-        self.time_bandwith = 8
-        self.time_unit = _np.timedelta64(1, "W")
+        self.space_bandwidth = 400 / 50
+        self.time_bandwidth = 8
 
-    def _gridsize(self, cell):
-        gridsize = cell.xmax - cell.xmin
-        if cell.ymax - cell.ymin != gridsize:
-            raise ValueError("Expect cells to be square.")
-        return gridsize
-
-    def _cell(self, x, y, gridsize):
-        return _np.floor(x / gridsize), _np.floor(y / gridsize)
-
-    def distance(self, x1, y1, x2, y2):
-        """Distance in the grid.  Diagonal distances are one, so (1,1) and
-        (2,2) are adjacent points.  This equates to using an \ell^\infty norm"""
-        return max(abs(x1 - x2), abs(y1 - y2))
-
-    def __call__(self, cell, time_into_past, x, y):
-        time_delta = _np.floor(time_into_past / self.time_unit + 0.0001) + 1
-        if time_delta >= self.time_bandwith:
-            return 0
-        
-        gridsize = self._gridsize(cell)
-        cellx, celly = self._cell((cell.xmin + cell.xmax) / 2,
-                                  (cell.ymin + cell.ymax) / 2, gridsize)
-        gx, gy = self._cell(x, y, gridsize)
-        space_delta = self.distance(cellx, celly, gx, gy) + 1
-        space_cutoff = _np.floor(self.space_bandwidth / gridsize)
-        if space_delta >= space_cutoff:
-            return 0
-
-        return 1 / (space_delta * time_delta)
+    def __call__(self, dt, dd):
+        mask = (dt < self.time_bandwidth) & (dd < self.space_bandwidth)
+        return 1 / ( (1 + dd) * ( 1 + dt) ) * mask
 
 
-class ClassicDiagonalsDifferent(ClassicDiagonalsSame):
-    def distance(self, x1, y1, x2, y2):
-        """Distance in the grid.  Now diagonal distances are two, so (1,1) and
-        (2,2) are two grid cells apart.  This equates to using an \ell^1 norm."""
-        return abs(x1 - x2) + abs(y1 - y2)
+class GridDistance(metaclass=_abc.ABCMeta):
+    """Calculates the distance between grid cells"""
+    @_abc.abstractmethod
+    def __call__(self, x1, y1, x2, y2):
+        pass
+
+
+class DistanceDiagonalsSame(GridDistance):
+    """Distance in the grid.  Diagonal distances are one, so (1,1) and
+    (2,2) are adjacent points.  This equates to using an \ell^\infty norm.
+    """
+    def __call__(self, x1, y1, x2, y2):
+        xx = _np.abs(x1 - x2)
+        yy = _np.abs(y1 - y2)
+        return _np.max(_np.vstack((xx, yy)), axis=0)
+
+
+class DistanceDiagonalsDifferent(GridDistance):
+    """Distance in the grid.  Now diagonal distances are two, so (1,1) and
+    (2,2) are two grid cells apart.  This equates to using an \ell^1 norm.
+    """
+    def __call__(self, x1, y1, x2, y2):
+        return _np.abs(x1 - x2) + _np.abs(y1 - y2)
 
 
 class ProspectiveHotSpot(predictors.DataTrainer):
-    def __init__(self, region):
-        self.grid = 50
-        self.region = region
-        self.weight = ClassicDiagonalsSame()
+    """Implements the classical, grid based algorithm.  To calculate distances,
+    we consider the grid cell we are computing the risk intensity for, the grid
+    cell the event falls into, and then delegate to an instance of :class
+    GridDistance: to compute the distance.  To compute time, we look at the
+    time difference between the prediction time and the timestamp of the event
+    and then divide by the :attribute time_unit:, then round down to the
+    nearest whole number.  So 6 days divided by 1 week is 0 whole units.
 
-    def _total_weight(self, cell, time_deltas, coords):
-        return sum(
-            self.weight(cell, t, x, y)
-            for t, x, y in zip(time_deltas, coords[0], coords[1]) )
+    Set :attribute distance: to change the computation of distance between
+    grid cells.  Set :attribute weight: to change the weight used.
+
+    :param region: The :class data.RectangularRegion: the data is in.
+    :param grid_size: The size of the grid to place the data into.
+    :param time_unit: A :class numpy.timedelta64: instance giving the time unit.
+    """
+    def __init__(self, region, grid_size=50, time_unit=timedelta64(1, "W")):
+        self.grid = grid_size
+        self.time_unit = time_unit
+        self.region = region
+        self.weight = ClassicWeight()
+        self.distance = DistanceDiagonalsSame()
+
+    def _cell(self, x, y):
+        gridx = _np.floor((x - self.region.xmin) / self.grid)
+        gridy = _np.floor((y - self.region.ymin) / self.grid)
+        return gridx, gridy
+
+    def _total_weight(self, time_deltas, coords, cellx, celly):
+        gridx, gridy = self._cell(coords[0], coords[1])
+        distances = self.distance(gridx, gridy, cellx, celly)
+        return _np.sum(self.weight(time_deltas, distances))
 
     def predict(self, cutoff_time, predict_time):
+        """Calculate a grid based prediction.
+
+        :param cutoff_time: Ignore data with a timestamp after this time.
+        :param predict_time: Timestamp of the prediction.  Used to calculate
+        the time difference between events and "now".  Typically the same as
+        `cutoff_time`.
+
+        :return: An instance of :class GridPredictionArray:
+        """
         if not cutoff_time <= predict_time:
             raise ValueError("Data cutoff point should be before prediction time")
         events = self.data.events_before(cutoff_time)
         time_deltas = _np.datetime64(predict_time) - events.timestamps
-        coords = events.coords
+        time_deltas = _np.floor(time_deltas / self.time_unit)
+
         width = int(_np.rint((self.region.xmax - self.region.xmin) / self.grid))
         height = int(_np.rint((self.region.ymax - self.region.ymin) / self.grid))
         matrix = _np.empty((height, width))
-        cell_outline = data.RectangularRegion(0, self.grid, 0, self.grid)
         for x in range(width):
             for y in range(height):
-                position = data.Point(x * self.grid, y * self.grid)
-                cell = cell_outline + position + self.region.min
-                matrix[y][x] = self._total_weight(cell, time_deltas, coords)
+                matrix[y][x] = self._total_weight(time_deltas, events.coords, x, y)
         return predictors.GridPredictionArray(self.grid, self.grid, matrix,
                                               self.region.xmin, self.region.ymin)
--- tests/prohotspot_test.py
+++ tests/prohotspot_test.py
@@ -1,5 +1,4 @@
 import pytest
-from pytest import approx
 import open_cp.prohotspot as testmod
 
 import numpy as np
@@ -7,215 +6,103 @@ from datetime import datetime, timedelta
 from unittest import mock
 import open_cp
 
-Cell = open_cp.RectangularRegion
+# --- Distance tests ---
 
-def test_weight_diags_same_distance():
-    weight = testmod.ClassicDiagonalsSame()
-    assert( weight.distance(1, 1, 2, 2) == 1 )
-    assert( weight.distance(1, 1, 1, 2) == 1 )
-    assert( weight.distance(1, 1, 1, 1) == 0 )
-    assert( weight.distance(2, 1, 1, 2) == 1 )
-    assert( weight.distance(2, 1, 4, 3) == 2 )
+def test_DistanceDiagonalsSame():
+    distance = testmod.DistanceDiagonalsSame()
+    assert( distance(1, 1, 2, 2) == 1 )
+    assert( distance(1, 1, 1, 2) == 1 )
+    assert( distance(1, 1, 1, 1) == 0 )
+    assert( distance(2, 1, 1, 2) == 1 )
+    assert( distance(2, 1, 4, 3) == 2 )
     
+def test_DistanceDiagonalsDifferent():
+    distance = testmod.DistanceDiagonalsDifferent()
+    assert( distance(1, 1, 2, 2) == 2 )
+    assert( distance(1, 1, 1, 2) == 1 )
+    assert( distance(1, 1, 2, 1) == 1 )
+    assert( distance(1, 1, 4, 5) == 7 )
+    
+def test_ClassicWeight():
+    weight = testmod.ClassicWeight()
+    weight.time_bandwidth = 5
+    
+    assert( weight(5, 0) == 0 )
+    assert( weight(0, 8) == 0 )
+    for i in range(5):
+        assert( weight(i, 0) == pytest.approx(1/(i+1)) )
+    for i in range(8):
+        assert( weight(0, i) == pytest.approx(1/(i+1)) )
+    assert( weight(1, 1) == pytest.approx(1/4) )
+    assert( weight(2, 1) == pytest.approx(1/6) )
+    assert( weight(1, 2) == pytest.approx(1/6) )
+    
+def __test_ClassicWeight_vectorised():
+    weight = testmod.ClassicWeight()
+    weight.space_bandwidth = 5
+    dt = np.asarray([8,0,0,1,2,3,4,5,6,7,1,2])
+    dd = np.asarray([0,5,0,0,0,0,0,0,0,0,1,1])
+    result = weight(dt, dd)
+    np.testing.assert_allclose(result, [0,0,1,1/2,1/3,1/4,1/5,1/6,1/7,1/8,1/4,1/6])
 
-def test_weight_diags_same_weight():
-    weight = testmod.ClassicDiagonalsSame()
-    cell = Cell(10, 20, 50, 60)
-    
-    # Too far in past
-    assert( weight(cell, np.timedelta64(8, "W"), 0, 0) == 0 )
-    
-    # Too distant
-    assert( weight(cell, np.timedelta64(1, "W"), 1000, 0) == 0 )
-    
-    # Same cell and time
-    assert( weight(cell, np.timedelta64(0), 15, 55) == 1 )
-    assert( weight(cell, np.timedelta64(0), 10, 50) == 1 )
-    assert( weight(cell, np.timedelta64(0), 20 - 0.1, 60 - 0.1) == 1 )
-    
-    # Time tests
-    assert( weight(cell, np.timedelta64(1, "W"), 15, 55) == approx(0.5) )
-    assert( weight(cell, np.timedelta64(9, "D"), 15, 55) == approx(0.5) )
-    assert( weight(cell, np.timedelta64(13, "D"), 15, 55) == approx(0.5) )
-    assert( weight(cell, np.timedelta64(14, "D"), 15, 55) == approx(1 / 3) )
-    assert( weight(cell, np.timedelta64(20, "D"), 15, 55) == approx(1 / 3) )
-    assert( weight(cell, np.timedelta64(21, "D"), 15, 55) == approx(1 / 4) )
-    assert( weight(cell, np.timedelta64(7 * 7 - 1, "D"), 15, 55) == approx(1 / 7) )
-    assert( weight(cell, np.timedelta64(7 * 7, "D"), 15, 55) == 0 )
-    
-    # Space tests
-    assert( weight(cell, np.timedelta64(0), 20, 55) == approx(1 / 2) )
-    assert( weight(cell, np.timedelta64(0), 30, 55) == approx(1 / 3) )
-    assert( weight(cell, np.timedelta64(0), 40, 55) == approx(1 / 4) )
-    assert( weight(cell, np.timedelta64(0), 399, 55) == approx(1 / 39) )
-    assert( weight(cell, np.timedelta64(0), 400, 55) == 0 )
-    
-    # Combined
-    assert( weight(cell, np.timedelta64(1, "W"), 20, 65) == approx(1 / 4) )
-    assert( weight(cell, np.timedelta64(3, "W"), 30, 65) == approx(1 / (4 * 3)) )
-    
-    
-def test_weight_diags_different_distance():
-    weight = testmod.ClassicDiagonalsDifferent()
-    assert( weight.distance(1, 1, 2, 2) == 2 )
-    assert( weight.distance(1, 1, 1, 2) == 1 )
-    assert( weight.distance(1, 1, 2, 1) == 1 )
-    assert( weight.distance(1, 1, 4, 5) == 7 )
-    
-    
 def test_predict_wrong_times():
-    pred = testmod.ProspectiveHotSpot(Cell(0, 0, 0, 0))
+    pred = testmod.ProspectiveHotSpot(None)
     with pytest.raises(ValueError):
         pred.predict(datetime(2017, 3, 10, 12, 30), datetime(2017, 3, 10, 0))
 
-def test_predict_single_event():
-    region = open_cp.RectangularRegion(0, 150, 0, 150)
-    cutoff = datetime(2017, 3, 10, 12, 30)
-    predict = cutoff
-    times = [cutoff]
-    coords = np.array([75, 75]).reshape((2,1))
-    
-    pred = testmod.ProspectiveHotSpot(region)
-    pred.data = open_cp.TimedPoints(times, coords)
-    prediction = pred.predict(cutoff, predict)
-    
-    wanted = [ [0.5, 0.5, 0.5], [0.5, 1, 0.5], [0.5, 0.5, 0.5] ]
-    for i in range(3):
-        for j in range(3):
-            assert( prediction.grid_risk(i, j) == wanted[j][i] )
-            
-    assert( prediction.grid_risk(3, 0) == 0 )
-    assert( prediction.grid_risk(0, 3) == 0 )
-    
-def test_predict_filter_by_time():
-    region = open_cp.RectangularRegion(0, 100, 0, 100)
-    cutoff = datetime(2017, 3, 10, 12, 30)
-    predict = datetime(2017, 3, 17, 12, 30)
-    times = [cutoff, cutoff + timedelta(hours = 5)]
-    coords = [ [25, 75], [25, 75] ]
-    data = open_cp.TimedPoints(times, coords)
-    
-    pred = testmod.ProspectiveHotSpot(region)
-    pred.data = data
-    prediction = pred.predict(cutoff, predict)
-    
-    assert( prediction.grid_risk(0, 0) == approx(1 / 2) )
-    assert( prediction.grid_risk(0, 1) == approx(1 / 4) )
-    assert( prediction.grid_risk(1, 0) == approx(1 / 4) )
-    assert( prediction.grid_risk(1, 1) == approx(1 / 4) )
-
-def test_predict_multiple_events():
-    region = open_cp.RectangularRegion(0, 100, 0, 100)
-    cutoff = datetime(2017, 3, 15, 12, 30)
-    predict = datetime(2017, 3, 17, 12, 30)
-    times = [cutoff - timedelta(days = 5), cutoff]
-    coords = [ [25, 75], [25, 75] ]
-    data = open_cp.TimedPoints(times, coords)
-    
-    pred = testmod.ProspectiveHotSpot(region)
-    pred.data = data
-    prediction = pred.predict(cutoff, predict)
-    
-    assert( prediction.grid_risk(0, 0) == approx(1 / 2 + 1 / 2) )
-    assert( prediction.grid_risk(0, 1) == approx(1 / 4 + 1 / 2) )
-    assert( prediction.grid_risk(1, 0) == approx(1 / 4 + 1 / 2) )
-    assert( prediction.grid_risk(1, 1) == approx(1 / 4 + 1) )
-
-
-# Aim is to now test the predictition algorithm.  If we believe the weight code
-# is correct (which we do!) then we can supply our own simplified weight and
-# test that the predict code does the right thing with it.
-
-class TestWeightConstant(testmod.Weight):
-    def __call__(self, cell, timestamp, x, y):
-        return 1
-
 def a_valid_predictor():
-    region = open_cp.RectangularRegion(0,100,0,144)
+    region = open_cp.RectangularRegion(0,150,0,150)
     predictor = testmod.ProspectiveHotSpot(region)
-    predictor.weight = TestWeightConstant()
-    predictor.grid = 10
     timestamps = [datetime(2017,3,1)]
     xcoords = [50]
     ycoords = [50]
     predictor.data = open_cp.TimedPoints.from_coords(timestamps, xcoords, ycoords)
     return predictor
 
-def test_ProspectiveHotSpot_cannot_pass_wrong_data_type():
-    p = a_valid_predictor()
-    with pytest.raises(TypeError):
-        p.data = [1,2,3]
-
-def test_ProspectiveHotSpot_times_ordered():
-    p = a_valid_predictor()
-    with pytest.raises(ValueError):
-        p.predict(datetime(2017,1,1), datetime(2016,1,1))
-
 def test_ProspectiveHotSpot_correct_return():
     p = a_valid_predictor()
     prediction = p.predict(datetime(2017,3,2), datetime(2017,3,2))
-    # Grid size
-    assert( prediction.xsize == 10 )
-    assert( prediction.ysize == 10 )
-    # Weight is uniformly 1.
-    for i in range(10):
-        for j in range(14):
-            assert( prediction.grid_risk(i, j) == 1 )
+    assert( prediction.xsize == 50 )
+    assert( prediction.ysize == 50 )
+    expected = np.zeros((3,3)) + 1/2
+    expected[1][1] = 1
+    for i in range(3):
+        for j in range(3):
+            assert( prediction.grid_risk(i, j) == expected[j][i] )
     assert( prediction.grid_risk(-1,0) == 0 )
-    assert( prediction.grid_risk(10,0) == 0 )
-    assert( prediction.grid_risk(0,14) == 0 )
+    assert( prediction.grid_risk(3,0) == 0 )
+    assert( prediction.grid_risk(0,3) == 0 )
 
 def test_ProspectiveHotSpot_filters_by_time():
     p = a_valid_predictor()
     prediction = p.predict(datetime(2017,2,1), datetime(2017,3,10))
     assert( prediction.grid_risk(0,0) == 0 )
 
-def test_ProspectiveHotSpot_weight_gets_time_delta():
+def test_ProspectiveHotSpot_uses_predict_time():
     p = a_valid_predictor()
-    p.weight = mock.Mock(return_value = 1)
-    p.predict(datetime(2017,3,1), datetime(2017,3,4,12,30))
+    expected = np.zeros((3,3)) + 1/2
+    expected[1][1] = 1
 
-    expected = timedelta(days=3, hours=12, minutes=30)
-    for call_args in p.weight.call_args_list:
-        assert( call_args[0][1] == np.timedelta64(expected) )
-    assert( p.weight.call_count == 140 )
-
-def test_ProspectiveHotSpot_gets_correct_cell_outline_and_coords():
-    region = open_cp.RectangularRegion(5,100,7,144)
-    predictor = testmod.ProspectiveHotSpot(region)
-    predictor.grid = 15
-    timestamps = [datetime(2017,3,1)]
-    xcoords = [50]
-    ycoords = [70]
-    predictor.data = open_cp.TimedPoints.from_coords(timestamps, xcoords, ycoords)
+    prediction = p.predict(datetime(2017,3,2), datetime(2017,3,7))
+    np.testing.assert_allclose(prediction.intensity_matrix, expected)
     
-    predictor.weight = mock.Mock(return_value = 1)
-    predictor.predict(datetime(2017,3,1), datetime(2017,3,4,12,30))
+    prediction = p.predict(datetime(2017,3,2), datetime(2017,3,8))
+    np.testing.assert_allclose(prediction.intensity_matrix, expected * 0.5)
 
-    for call_args in predictor.weight.call_args_list:
-        assert( call_args[0][2] == 50 )
-        assert( call_args[0][3] == 70 )
-        cell = call_args[0][0]
-        assert( (cell.xmin - 5) % 15 == 0 )
-        assert( (cell.ymin - 7) % 15 == 0 )
-        assert( cell.xmax - cell.xmin == 15 )
-        assert( cell.ymax - cell.ymin == 15 )
+    prediction = p.predict(datetime(2017,3,2), datetime(2017,3,14))
+    np.testing.assert_allclose(prediction.intensity_matrix, expected * 0.5)
 
-def test_ProspectiveHotSpot_record_total_weight():
-    predictor = a_valid_predictor()
+    prediction = p.predict(datetime(2017,3,2), datetime(2017,3,15))
+    np.testing.assert_allclose(prediction.intensity_matrix, expected * (1/3))
+
+def test_ProspectiveHotSpot_additive():
+    p = a_valid_predictor()
     timestamps = [datetime(2017,3,1), datetime(2017,3,1)]
-    xcoords = [52, 72]
-    ycoords = [52, 25]
-    predictor.data = open_cp.TimedPoints.from_coords(timestamps, xcoords, ycoords)
+    xcoords = [49, 101]
+    ycoords = [49, 101]
+    p.data = open_cp.TimedPoints.from_coords(timestamps, xcoords, ycoords)
 
-    class TestWeight(testmod.Weight):
-        def __call__(self, cell, timestamp, x, y):
-            if cell.xmin < x and x < cell.xmax and cell.ymin < y and y < cell.ymax:
-                return 1 if x == 52 else 2
-            return 0.5
-    predictor.weight = TestWeight()
-        
-    prediction = predictor.predict(datetime(2017,3,1), datetime(2017,3,1))
-    assert( prediction.grid_risk(5, 5) == 1.5 )
-    assert( prediction.grid_risk(7, 2) == 2.5 )
-    assert( prediction.grid_risk(0, 0) == 1.0 )
+    expected = np.asarray([[1,1/2,1/3], [1/2,1/2,1/3], [1/3,1/3,1/3]])
+    expected += np.asarray([[1/3,1/3,1/3], [1/3,1/2,1/2], [1/3,1/2,1]])
+    prediction = p.predict(datetime(2017,3,2), datetime(2017,3,2))
+    np.testing.assert_allclose(prediction.intensity_matrix, expected)
