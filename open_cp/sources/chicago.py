7850138389afaa3c8e0b96d8420a5fd19fe79fe1
Matthew Daws <matt.daws@cantab.net>
2017-05-12 08:05:18 +0100

    Enrich chicago source

--- open_cp/sources/chicago.py
+++ open_cp/sources/chicago.py
@@ -8,10 +8,13 @@ Reads a CSV file in the format (as of April 2017) of data available from:
 - https://catalog.data.gov/dataset/crimes-2001-to-present-398a4
 
 The default data is loaded from a file "chicago.csv" which should be downloaded
-from one of the above links.
+from one of the above links.  The format of the data, frustratingly, differs
+between the snapshot of last year, and the total.
 
 The data is partly anonymous in that the address within a block is obscured,
-and the geocoding always returns a coordinate in the middle of a block.
+while the geocoding seems complicated (work in progress to understand)...
+
+The crime type "HOMICIDE" is reported multiple times in the dataset.
 """
 
 import csv
@@ -21,19 +24,49 @@ import numpy as np
 from ..data import TimedPoints
 
 _default_filename = os.path.join(os.path.split(__file__)[0],"chicago.csv")
-_DESCRIPTION_FIELD = ' PRIMARY DESCRIPTION'
-_X_FIELD = 'X COORDINATE'
-_Y_FIELD = 'Y COORDINATE'
-_TIME_FIELD = 'DATE  OF OCCURRENCE'
+
 _FEET_IN_METERS = 3.28084
+
+_FIELDS = {
+    "snapshot" : {
+        "_DESCRIPTION_FIELD" : ' PRIMARY DESCRIPTION',
+        "_X_FIELD" : 'X COORDINATE',
+        "_Y_FIELD" : 'Y COORDINATE',
+        "_TIME_FIELD" : 'DATE  OF OCCURRENCE',
+
+        "_GEOJSON_LOOKUP" : {"case": 'CASE#',
+            "address": "BLOCK",
+            "location": ' LOCATION DESCRIPTION',
+            "crime": ' PRIMARY DESCRIPTION',
+            "type": ' SECONDARY DESCRIPTION',
+            "timestamp": 'DATE  OF OCCURRENCE'},
+        "GEOJSON_COORDS" : ('LONGITUDE', 'LATITUDE')
+    },
+    "all" : {
+        "_DESCRIPTION_FIELD" : 'Primary Type',
+        "_X_FIELD" : 'X Coordinate',
+        "_Y_FIELD" : 'Y Coordinate',
+        "_TIME_FIELD" : 'Date',
+
+        "_GEOJSON_LOOKUP" : {"case": 'Case Number',
+            "address": "Block",
+            "location": 'Location Description',
+            "crime": 'Primary Type',
+            "type": 'Description',
+            "timestamp": 'Date'},
+        "GEOJSON_COORDS" : ('Longitude', 'Latitude')
+    }
+}
 
 def _date_from_csv(date_string):
     return datetime.datetime.strptime(date_string, "%m/%d/%Y %I:%M:%S %p")
-    raise Exception("This: '{}'".format(date_string))
 
-def _convert_header(header):
+def _date_from_iso(iso_string):
+    return datetime.datetime.strptime(iso_string, "%Y-%m-%dT%H:%M:%S")
+
+def _convert_header(header, dic):
     lookup = dict()
-    for field in [_DESCRIPTION_FIELD, _X_FIELD, _Y_FIELD, _TIME_FIELD]:
+    for field in [dic["_DESCRIPTION_FIELD"], dic["_X_FIELD"], dic["_Y_FIELD"], dic["_TIME_FIELD"]]:
         if not field in header:
             raise Exception("No field '{}' found in header".format(field))
         lookup[field] = header.index(field)
@@ -49,28 +82,38 @@ def default_burglary_data():
     except Exception:
         return None
 
-def load(filename, primary_description_names, to_meters=True):
+def _get_dic(type):
+    try:
+        return _FIELDS[type]
+    except KeyError:
+        raise ValueError("Don't understand type {}".format(type))
+
+def load(filename, primary_description_names, to_meters=True, type="snapshot"):
     """Load data from a CSV file in the expected format.
 
     :param filename: Name of the CSV file load.
     :param primary_description_names: Set of names to search for in the
       "primary description field". E.g. pass `{"THEFT"}` to return only the
       "theft" crime type.
+    :param to_meters: Convert the coordinates to meters; True by default.
+    :param type: Either "snapshot" or "all" depending on whether the data
+      has headers conforming the the data "last year" or "2001 to present".
 
     :return: An instance of :class:`open_cp.data.TimedPoints` or `None`.
     """
+    dic = _get_dic(type)
     data = []
 
     with open(filename) as file:
         reader = csv.reader(file)
-        lookup = _convert_header(next(reader))
+        lookup = _convert_header(next(reader), dic)
         for row in reader:
-            description = row[lookup[_DESCRIPTION_FIELD]].strip()
+            description = row[lookup[dic["_DESCRIPTION_FIELD"]]].strip()
             if not description in primary_description_names:
                 continue
-            x = row[lookup[_X_FIELD]].strip()
-            y = row[lookup[_Y_FIELD]].strip()
-            t = row[lookup[_TIME_FIELD]].strip()
+            x = row[lookup[dic["_X_FIELD"]]].strip()
+            y = row[lookup[dic["_Y_FIELD"]]].strip()
+            t = row[lookup[dic["_TIME_FIELD"]]].strip()
             if x != "" and y != "":
                 data.append((_date_from_csv(t), float(x), float(y)))
 
@@ -84,3 +127,87 @@ def load(filename, primary_description_names, to_meters=True):
         xcoords /= _FEET_IN_METERS
         ycoords /= _FEET_IN_METERS
     return TimedPoints.from_coords(times, xcoords, ycoords)
+
+def _convert_header_for_geojson(header, dic):
+    try:
+        column_lookup = {}
+        for key, col_head in dic["_GEOJSON_LOOKUP"].items():
+            column_lookup[key] = header.index(col_head)
+        coord_lookup = [header.index(chead) for chead in dic["GEOJSON_COORDS"]]
+        return column_lookup, coord_lookup
+    except KeyError as ex:
+        raise ValueError("Header not in expected format: {} caused by {}/{}".format(
+            header, type(ex), ex))
+
+def generate_GeoJSON_Features(filename=_default_filename, type="snapshot"):
+    """Generate a sequence of GeoJSON "features" from the CSV file.
+    See :func:`load_to_GeoJSON`.
+    """
+    dic = _get_dic(type)
+    with open(filename) as file:
+        reader = csv.reader(file)
+        column_lookup, coord_lookup = _convert_header_for_geojson(next(reader), dic)
+        for row in reader:
+            properties = {key : row[i] for key, i in column_lookup.items()}
+            properties["timestamp"] = _date_from_csv(properties["timestamp"]).isoformat()
+            if row[coord_lookup[0]] == "":
+                geometry = None
+            else:
+                coordinates = [float(row[i]) for i in coord_lookup]
+                geometry = {"type":"Point", "coordinates":coordinates}
+            yield {"geometry": geometry, "properties": properties,
+                    "type": "Feature"}
+
+def load_to_GeoJSON(filename=_default_filename, type="snapshot"):
+    """Load the specified CSV file to a list of GeoJSON (see
+    http://geojson.org/) features.  Events with no location data have `None`
+    as the geometry.  Timestamps are converted to standard ISO string format.
+
+    The returned "properties" have these keys:
+    - "case" for the "CASE#" field
+    - "crime" for the "PRIMARY DESCRIPTION" field
+    - "type" for the "SECONDARY DESCRIPTION" field
+    - "location" for the  "LOCATION DESCRIPTION" field
+    - "timestamp" for the "DATE  OF OCCURRENCE" field
+    - "address" for the "BLOCK" field
+
+    :param filename: Filename of the CSV file to process
+    :param type: Either "snapshot" or "all" depending on whether the data
+      has headers conforming the the data "last year" or "2001 to present".
+
+    :return: List of Python dictionaries in GeoJSON format.
+    """
+    return list(generate_GeoJSON_Features(filename, type))
+
+try:
+    import geopandas as gpd
+    import shapely.geometry as _geometry
+except:
+    gpd = None
+    _geometry = None
+
+def load_to_geoDataFrame(filename=_default_filename, datetime_as_string=True, type="snapshot"):
+    """Return the same data as :func:`load_to_GeoJSON` but as a geoPandas
+    data-frame.
+
+    :param filename: Filename of the CSV file to process
+    :param datetime_as_string: Write the timestamp as an ISO formatted string.
+      Defaults to True which is best for saving the dataframe as e.g. a shape
+      file.  Set to False to get timestamps as python objects, which is best
+      for using (geo)pandas to analyse the data.
+    :param type: Either "snapshot" or "all" depending on whether the data
+      has headers conforming the the data "last year" or "2001 to present".
+
+    """
+    geo_data = load_to_GeoJSON(filename, type=type)
+    if not datetime_as_string:
+        for feature in geo_data:
+            feature["properties"]["timestamp"] = _date_from_iso(feature["properties"]["timestamp"])
+    frame = gpd.GeoDataFrame.from_features(geo_data)
+    def none_to_point(x):
+        if x is None:
+            return _geometry.Point()
+        return x
+    frame.geometry = frame.geometry.map(none_to_point)
+    frame.crs = {"init":"EPSG:4326"}
+    return frame
--- tests/sources/chicago_test.py
+++ tests/sources/chicago_test.py
@@ -12,16 +12,19 @@ def test_load_default_filename():
         filename = open_mock.calls[0][0][0]
         assert( os.path.split(filename)[1] == "chicago.csv" )
 
-string_data = "\n".join([
-    ",".join([chicago._DESCRIPTION_FIELD, chicago._X_FIELD, chicago._Y_FIELD,
-        "other", chicago._TIME_FIELD]),
+@pytest.fixture
+def string_data_snap():
+    dic = chicago._FIELDS["snapshot"]
+    return "\n".join([
+    ",".join([dic["_DESCRIPTION_FIELD"], dic["_X_FIELD"], dic["_Y_FIELD"],
+        "other", dic["_TIME_FIELD"]]),
     "THEFT, 789, 1012, ahgd, 01/01/2017 10:30:23 PM",
     "ASSAULT, 12, 34, dgs, sgjhg",
     "THEFT, 123, 456, as, 03/13/2016 02:53:30 AM"
     ])
 
-def test_load_data():
-    with mock.patch("builtins.open", MockOpen(string_data)) as open_mock:
+def test_load_data(string_data_snap):
+    with mock.patch("builtins.open", MockOpen(string_data_snap)) as open_mock:
         points = chicago.load("filename", {"THEFT"})
         assert( open_mock.calls[0][0] == ("filename",) )
 
@@ -31,8 +34,8 @@ def test_load_data():
         np.testing.assert_allclose( points.coords[:,0], np.array([123, 456]) / 3.28084 )
         np.testing.assert_allclose( points.coords[:,1], np.array([789, 1012]) / 3.28084 )
 
-def test_load_data_keep_in_feet():
-    with mock.patch("builtins.open", MockOpen(string_data)) as open_mock:
+def test_load_data_keep_in_feet(string_data_snap):
+    with mock.patch("builtins.open", MockOpen(string_data_snap)) as open_mock:
         points = chicago.load("filename", {"THEFT"}, to_meters=False)
         np.testing.assert_allclose( points.coords[:,0], [123, 456] )
         np.testing.assert_allclose( points.coords[:,1], [789, 1012] )
