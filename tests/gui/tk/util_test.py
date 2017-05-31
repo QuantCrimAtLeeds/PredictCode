9550c0574917a842541ba3a71309c17f89da1578
Matthew Daws <matmdpd@leeds.ac.uk>
2017-05-31 13:08:13 +0100

    Allow to work headless

--- tests/gui/tk/util_test.py
+++ tests/gui/tk/util_test.py
@@ -2,7 +2,15 @@ import pytest
 
 import open_cp.gui.tk as tk
 
-def test_screen_size():
-    root = tk.tk.Tk()
+@pytest.fixture
+def root():
+    try:
+        root = tk.tk.Tk()
+        return root
+    except tk.tk.TclError:
+        pass
+
+def test_screen_size(root):
+    if root is None: return
     w, h = tk.screen_size(root)
     assert((w,h) == (1280,1024))
