6e5199275cb9a86c8e40312b7af0f478400d09e7
Matthew Daws <matt.daws@cantab.net>
2017-06-08 15:55:20 +0100

    This should now work.

--- open_cp/gui/tk/date_picker.py
+++ open_cp/gui/tk/date_picker.py
@@ -15,15 +15,87 @@ import tkinter.ttk as ttk
 import open_cp.gui.tk.util as util
 import datetime
 
-class DatePicker(tk.Frame):
+class DatePicker():
     def __init__(self, parent=None):
-        super().__init__(parent)
-        
         self._first_day = "Mon"
         now = datetime.date.today()
         self._month_year = (now.month, now.year)
         self._selected_date = now
-        
+        self._selected_colour = "#6666ff"
+
+        self._view = _DatePickerView(parent, self)
+
+    def day_to_text(self, day):
+        """0 = Monday, 1 = Tuesday and so forth"""
+        if day < 0 or day > 6:
+            raise ValueError()
+        d = datetime.date(year=2017, month=6, day=5+day)
+        return d.strftime("%a")
+
+    @property
+    def widget(self):
+        """Get the :class:`tk.Frame` widget which contains the date picker."""
+        return self._view
+
+    @property
+    def first_day_of_week(self):
+        """The first day of the week, either "Mon" or "Sun"."""
+        return self._first_day
+
+    @first_day_of_week.setter
+    def first_day_of_week(self, value):
+        if value not in {"Mon", "Sun"}:
+            raise ValueError("Should be 'Mon' or 'Sun'.")
+        self._first_day = value
+        self._view.make_day_labels()
+        self._view.make_date_grid()
+
+    @property
+    def month_year(self):
+        """The currently viewed page `(month, year)` as integers, with 1 = January,
+        2 = February etc."""
+        return self._month_year
+
+    @month_year.setter
+    def month_year(self, value):
+        try:
+            try:
+                month, year = value
+            except Exception:
+                raise ValueError()
+            month, year = int(month), int(year)
+            if month < 1 or month > 12:
+                raise ValueError
+            self._month_year = (month, year)
+            self._view.refresh_month_year()
+        except ValueError:
+            raise ValueError("Should be pair (month, year) of integers.")
+
+    @property
+    def selected_date(self):
+        """The currently selected date."""
+        return self._selected_date
+
+    @selected_date.setter
+    def selected_date(self, value):
+        try:
+            y, m, d = value.year, value.month, value.day
+            value = datetime.date(year=y, month=m, day=d)
+        except Exception:
+            raise ValueError("Not a valid date")
+        self._selected_date = value
+        self._view.refresh_month_year()
+
+    @property
+    def selected_colour(self):
+        """The colour of the button which indicates the selected date."""
+        return self._selected_colour
+
+
+class _DatePickerView(tk.Frame):
+    def __init__(self, parent, model):
+        super().__init__(parent)
+        self._model = model
         util.stretchy_columns(self, range(7))
         util.stretchy_rows(self, range(7))
         self._add_widgets()
@@ -38,55 +110,46 @@ class DatePicker(tk.Frame):
 
     def _left(self):
         """Move one month into past."""
-        m, y = self.month_year
+        m, y = self._model.month_year
         m -= 1
         if m == 0:
             y -= 1
             m = 12
-        self.month_year = (m, y)
-        self._refresh_month_year()
+        self._model.month_year = (m, y)
 
     def _right(self):
         """Move one month into future."""
-        m, y = self.month_year
+        m, y = self._model.month_year
         m += 1
         if m == 13:
             y += 1
             m = 1
-        self.month_year = (m, y)
-        self._refresh_month_year()
+        self._model.month_year = (m, y)
 
-    def day_to_text(self, day):
-        """0 = Monday, 1 = Tuesday and so forth"""
-        if day < 0 or day > 6:
-            raise ValueError()
-        d = datetime.date(year=2017, month=6, day=5+day)
-        return d.strftime("%a")
-
-    def _make_day_labels(self):
+    def make_day_labels(self):
         for label in self._day_labels:
             label.destroy()
         self._day_labels = []
-        if self.first_day_of_week == "Sun":
+        if self._model.first_day_of_week == "Sun":
             day = 6
         else:
             day = 0
         for c in range(7):
-            label = ttk.Label(self, text=self.day_to_text(day), anchor=tk.CENTER)
+            label = ttk.Label(self, text=self._model.day_to_text(day), anchor=tk.CENTER)
             label.grid(row=1, column=c, sticky=tk.EW)
             self._day_labels.append(label)
             day = (day + 1) % 7
 
     def _day_to_column(self, day):
         """ValueError on day out of range."""
-        m, y = self.month_year
+        m, y = self._model.month_year
         d = datetime.date(year = y, month = m, day = day)
         column = d.weekday()
-        if self.first_day_of_week == "Sun":
+        if self._model.first_day_of_week == "Sun":
             column = (column + 1) % 7
         return column
 
-    def _make_date_grid(self):
+    def make_date_grid(self):
         for b in self._day_buttons:
             b.destroy()
         self._day_buttons = []
@@ -112,24 +175,24 @@ class DatePicker(tk.Frame):
             self._day_buttons.append(b)
 
         self._bg_colour = self._day_buttons[0]["bg"]
-        if self.month_year == (self.selected_date.month, self.selected_date.year):
-            b = self._day_buttons[ self.selected_date.day - 1 ]
-            b["bg"] = "#8888ff"
+        if self._model.month_year == (self._model.selected_date.month, self._model.selected_date.year):
+            b = self._day_buttons[ self._model.selected_date.day - 1 ]
+            b["bg"] = self._model.selected_colour
 
     def _date_selected(self, day):
-        m, y = self.month_year
-        self._selected_date = datetime.date(year=y, month=m, day=day)
+        m, y = self._model.month_year
+        self._model._selected_date = datetime.date(year=y, month=m, day=day)
         for i, b in enumerate(self._day_buttons):
             if i + 1 == day:
-                b["bg"] = "#8888ff"
+                b["bg"] = self._model.selected_colour
             else:
                 b["bg"] = self._bg_colour
 
-    def _refresh_month_year(self):
-        m, y = self.month_year
+    def refresh_month_year(self):
+        m, y = self._model.month_year
         d = datetime.date(year=y, month=m, day=1)
         self._month_label["text"] = d.strftime("%b %Y")
-        self._make_date_grid()
+        self.make_date_grid()
 
     def _add_widgets(self):
         ttk.Button(self, text="<<", command=self._left).grid(row=0, column=1, sticky=tk.EW)
@@ -138,67 +201,19 @@ class DatePicker(tk.Frame):
         self._month_label.grid(row=0, column=2, columnspan=3, sticky=tk.EW)
         
         self._day_labels = []
-        self._make_day_labels()
+        self.make_day_labels()
 
         self._day_buttons = []
-        self._refresh_month_year()
-
-    @property
-    def first_day_of_week(self):
-        """The first day of the week, either "Mon" or "Sun"."""
-        return self._first_day
-
-    @first_day_of_week.setter
-    def first_day_of_week(self, value):
-        if value not in {"Mon", "Sun"}:
-            raise ValueError("Should be 'Mon' or 'Sun'.")
-        self._first_day = value
-        self._make_day_labels()
-        self._make_date_grid()
-
-    @property
-    def month_year(self):
-        """The currently viewed page `(month, year)` as integers, with 1 = January,
-        2 = February etc."""
-        return self._month_year
-
-    @month_year.setter
-    def month_year(self, value):
-        try:
-            try:
-                month, year = value
-            except Exception:
-                raise ValueError()
-            month, year = int(month), int(year)
-            if month < 1 or month > 12:
-                raise ValueError
-            self._month_year = (month, year)
-            self._refresh_month_year()
-        except ValueError:
-            raise ValueError("Should be pair (month, year) of integers.")
-
-    @property
-    def selected_date(self):
-        """The currently selected date."""
-        return self._selected_date
-
-    @selected_date.setter
-    def selected_date(self, value):
-        try:
-            y, m, d = value.year, value.month, value.day
-            value = datetime.date(year=y, month=m, day=d)
-        except Exception:
-            raise ValueError("Not a valid date")
-        self._selected_date = value
+        self.refresh_month_year()
 
 if __name__ == "__main__":
     root = tk.Tk()
     util.stretchy_columns(root, [0])
     util.stretchy_rows(root, [0])
     dp = DatePicker(root)
-    dp["borderwidth"] = 5
-    dp["relief"] = "groove"
-    dp.grid(sticky=tk.NSEW)
+    dp.widget["borderwidth"] = 5
+    dp.widget["relief"] = "groove"
+    dp.widget.grid(sticky=tk.NSEW)
     frame = tk.Frame(root)
     frame.grid()
 
@@ -208,10 +223,14 @@ if __name__ == "__main__":
         dp.first_day_of_week = "Mon"
     def go_month():
         dp.month_year = (1, 2016)
+    def select_tomorrow():
+        d = datetime.datetime.now() + datetime.timedelta(days=1)
+        dp.selected_date = d
 
     ttk.Button(frame, text="Quit", command=root.quit).grid(row=0, column=0)
     ttk.Button(frame, text="Sunday", command=sunday).grid(row=1, column=0)
     ttk.Button(frame, text="Monday", command=monday).grid(row=1, column=1)
     ttk.Button(frame, text="Jan 2016", command=go_month).grid(row=2, column=0)
+    ttk.Button(frame, text="Tomorrow", command=select_tomorrow).grid(row=2, column=1)
 
     root.mainloop()
--- tests/gui/tk/date_picker_test.py
+++ tests/gui/tk/date_picker_test.py
@@ -1,10 +1,12 @@
 import pytest
+import unittest.mock as mock
 
 import open_cp.gui.tk.date_picker as date_picker
 
 @pytest.fixture
 def dp():
-    return date_picker.DatePicker()
+    with mock.patch("open_cp.gui.tk.date_picker._DatePickerView") as clazzmock:
+        yield date_picker.DatePicker()
 
 def test_days_to_text(dp):
     import locale
@@ -27,6 +29,8 @@ def test_first_day_of_week(dp):
     assert dp.first_day_of_week == "Mon"
     dp.first_day_of_week = "Sun"
     assert dp.first_day_of_week == "Sun"
+    dp._view.make_day_labels.assert_called_once_with()
+    dp._view.make_date_grid.assert_called_once_with()
 
     with pytest.raises(ValueError):
         dp.first_day_of_week = "Tue"
@@ -36,8 +40,11 @@ def test_month_year(dp):
 
     dp.month_year = (1, 1987)
     assert dp.month_year == (1, 1987)
+    dp._view.refresh_month_year.assert_called_once_with()
+    
     dp.month_year = ("12", "2023")
     assert dp.month_year == (12, 2023)
+    
     with pytest.raises(ValueError):
         dp.month_year = 5
     with pytest.raises(ValueError):
@@ -52,6 +59,8 @@ def test_set_selected(dp):
 
     dp.selected_date = datetime.date(year=2011, month=5, day=23)
     assert dp.selected_date == datetime.date(year=2011, month=5, day=23)
+    dp._view.refresh_month_year.assert_called_once_with()
+
     with pytest.raises(ValueError):
         dp.selected_date = 5        
         
