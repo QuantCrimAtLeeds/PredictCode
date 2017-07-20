26712d7e3d0d7dc77df238d042048f4b89419b5b
Matthew Daws <matt.daws@cantab.net>
2017-07-20 09:23:02 +0100

    Issue 5

--- open_cp/logger.py
+++ open_cp/logger.py
@@ -11,37 +11,42 @@ Currently, few modules log.  Logging is always under the `__name__` of the modul
 import logging as _logging
 import sys as _sys
 
-_current_handler = None
+
+class _OurHandler(_logging.StreamHandler):
+    """Wrap :class:`logging.StreamHandler` with an attribute which we can
+    recognise, to stop adding more than one handler."""
+    def __init__(self, obj):
+        super().__init__(obj)
+        self.open_cp_marker = True
+
 
 def _set_handler(handler):
     logger = _logging.getLogger("open_cp")
-    global _current_handler
-    if _current_handler is not None:
-        logger.removeHandler(_current_handler)
-    _current_handler = handler
+    existing = [ h for h in logger.handlers if hasattr(h, "open_cp_marker") ]
+    for h in existing:
+        logger.removeHandler(h)
     logger.addHandler(handler)
 
 def standard_formatter():
     """Our standard logging formatter"""
     return _logging.Formatter("{asctime} {levelname} {name} - {message}", style="{")
 
+def _log_to(file):
+    logger = _logging.getLogger("open_cp")
+    logger.setLevel(_logging.DEBUG)
+    ch = _OurHandler(file)
+    ch.setFormatter(standard_formatter())
+    _set_handler(ch)
+
 def log_to_stdout():
     """Start logging to `stdout`.  In a Jupyter notebook, this will print
     logging to the notebook.
     """
-    logger = _logging.getLogger("open_cp")
-    logger.setLevel(_logging.DEBUG)
-    ch = _logging.StreamHandler(_sys.stdout)
-    ch.setFormatter(standard_formatter())
-    _set_handler(ch)
+    _log_to(_sys.stdout)
 
 def log_to_true_stdout():
     """Start logging to the "real" `stdout`.  In a Jupyter notebook, this will
     print logging to the console the server is running in (and not to the
     notebook) itself.
     """
-    logger = _logging.getLogger("open_cp")
-    logger.setLevel(_logging.DEBUG)
-    ch = _logging.StreamHandler(_sys.__stdout__)
-    ch.setFormatter(standard_formatter())
-    _set_handler(ch)
+    _log_to(_sys.__stdout__)
